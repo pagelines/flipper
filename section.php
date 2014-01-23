9496f14051d83814ba4d256f216ac53dd4f168d5
Simon Prosser <simon@pagelines.com>
2014-01-23 23:23:59 +0000

    cache id

--- section.php
+++ section.php
@@ -21,8 +21,8 @@ class PageLinesFlipper extends PageLinesSection {
 	}
 
 	function section_styles(){
-		wp_enqueue_script( 'caroufredsel', $this->base_url.'/caroufredsel.min.js', array( 'jquery' ), PL_CORE_VERSION, true );
-		wp_enqueue_script( 'flipper', $this->base_url.'/flipper.js', array( 'jquery' ), PL_CORE_VERSION, true );
+		wp_enqueue_script( 'caroufredsel', $this->base_url.'/caroufredsel.min.js', array( 'jquery' ), pl_get_cache_key(), true );
+		wp_enqueue_script( 'flipper', $this->base_url.'/flipper.js', array( 'jquery' ), pl_get_cache_key(), true );
 	}
 
 	function section_opts(){
